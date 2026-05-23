39504a647af39f1c462aac1d959b26dc596e4f88
Tanvir-Sorforaz <tanvirsf12345@gmail.com>
2026-05-23 16:29:48 +0600

    setup config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
+import config  from "./config/index";
 import express from "express"
 const app = express()
-const port = 3000
+const port = Number(config.port ?? 3000);
 
 app.get('/', (req, res) => {
   res.send('The server is running!')
